0434e5e8e2442863e17867cdcb6b41eb763c2573
Emiliano Silva Barbosa <emilianoeloi@gmail.com>
2015-03-08 22:52:31 -0300

    - Add KiF Test Framework; - Create a Unit tests for: Add, Edit, List and Delete register

---
+++ ios-ormTests/KIFUITestActor+Cinefilo.m
@@ -0,0 +1,13 @@
+//
+//  KIFUITestActor+Cinefilo.m
+//  ios-orm
+//
+//  Created by emilianoeloi on 3/8/15.
+//  Copyright (c) 2015 Emiliano Eloi. All rights reserved.
+//
+
+#import "KIFUITestActor+Cinefilo.h"
+
+@implementation KIFUITestActor (Cinefilo)
+
+@end
